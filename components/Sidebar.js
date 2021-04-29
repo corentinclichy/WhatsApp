ac222ce513b3dd8d8e35555e989dddc74dfaf578
Corentin Clichy <cor.clichy@gmail.com>
2021-04-29 08:37:45 -0400

    code sidebar and setting up firebase

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,32 @@
 import styled from "styled-components";
+import { Avatar, IconButton, Button } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChatIcon from "@material-ui/icons/Chat";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Sidebar() {
   return (
     <Container>
-      <Header></Header>
+      <Header>
+        <UserAvatar />
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
+      </Search>
+
+      <SideBarButton>Start a new chat</SideBarButton>
+
+      {/* List of Chats */}
     </Container>
   );
 }
@@ -12,4 +35,51 @@ export default Sidebar;
 
 const Container = styled.div``;
 
-const Header = styled.div``;
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const IconsContainer = styled.div``;
+
+//Search Style
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+
+//Start chat button style
+
+const SideBarButton = styled(Button)`
+  width: 100%;
+
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
+`;
